bea7944a1293a19c49c855f44a5f385fd4f78c43
Vikas S Biradar <164601893+VikasSBiradar@users.noreply.github.com>
2024-10-15 16:53:43 +0530

    Day 14

---
+++ 643_maximum_average_subarray_I(Day_14).cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public double FindMaxAverage(int[] nums, int k) {
+        double avg = 0.00;
+        double ans = 0.00;
+
+        int sum = 0;
+        for(int i = 0; i < k; i++){
+            sum += nums[i];
+        }
+        avg = sum * 1.0/ k;
+        ans = avg;
+
+        for(int i=k ; i< nums.Length; i++){
+            sum += nums[i] - nums[i-k];
+            avg = sum * 1.0/k;
+            ans = Math.Max(ans,avg);
+        }
+        return ans;
+    }
+}
