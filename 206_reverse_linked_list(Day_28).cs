e44a4b7f7370c78429c0edda28e2b3e51bd229f7
Vikas S Biradar <164601893+VikasSBiradar@users.noreply.github.com>
2024-11-13 18:23:04 +0530

    Day 28 challenge

---
+++ 206_reverse_linked_list(Day_28).cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        if(head == null) return null;
+        ListNode current = head, prev = null;
+
+        while(current != null){
+            ListNode temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+        return prev;
+    }
+}
+	
+	
